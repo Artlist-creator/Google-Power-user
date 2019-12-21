2dfb0d19a46ef0545efde4abce4cdb4bc240b7a8
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-12-21 00:54:29 -0800

    update my bash file in week-1

---
+++ Week-1/History.sh
@@ -0,0 +1,18 @@
+cd ..
+cd user/
+cd Documents/
+ls -lah
+cat /home/user/Desktop/colors
+mkdir red blue green yellow magenta
+cd /home/user/Pictures
+ls -a
+mv .apple .banana .broccoli .milk /home/user/Documents/Hidden
+mv /home/user/Movies/Europe\ Pictures /home/user/Pictures
+cd /home/user/Pictures
+mv /home/user/Images/Vacation.JPG
+mv /home/user/Images/Vacation.JPG .
+cd /home/user/Music
+rm Best_of_the_90s 80s_jams Classical
+rmdir Rock
+grep -rw /home/user/Downloads -e "vacation"
+mv /home/user/Downloads/Iceland /home/user/Downloads/Japan /home/user/Documents
