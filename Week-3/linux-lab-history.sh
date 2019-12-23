b71214916e46abf2f29f0ff5a14a07c8a20690e9
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-12-23 07:06:41 -0800

    Create linux-lab-history.sh

---
+++ Week-3/linux-lab-history.sh
@@ -0,0 +1,11 @@
+
+    1  sudo dpkg -i /home/qwiklab/downloads/atom-amd64.deb
+    2  sudo apt install -f
+    3  dpkg -s atom
+    4  cd /home/qwiklab/downloads
+    5  sudo tar -xvf extract_me.tar
+    6  cd ~
+    7  tar -cvf Planets.tar /home/qwiklab/documents/Earth /home/qwiklab/documents/Mercury /home/qwiklab/documents/Venus
+    8  sudo apt install p7zip-full -y
+    9  sudo apt remove gimp
+   10  history
