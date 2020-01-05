7301c2454fa6bbd9673a483acd834ad525913db2
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2020-01-05 07:05:46 -0500

    Create history.sh

---
+++ Week-5/history.sh
@@ -0,0 +1,7 @@
+   1  ps -aux | grep "totally_not_malicious"
+    2  sudo kill 1467
+    3  sudo kill 1473
+    4  ps -aux | grep "razzle"
+    5  sudo kill 1468 1469 1470
+    6  ps -aux | grep "razzle"
+    7  history
